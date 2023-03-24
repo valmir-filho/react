378f51d47a8be0de6e6cb4ca15ad98116c43b91d
Valmir <vmcfilho@gmail.com>
2023-03-24 16:00:00 -0300

    Update App.js

--- introdução/src/App.js
+++ introdução/src/App.js
@@ -1,1 +1,17 @@
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <h1>Testando Tags HTML: H1</h1>
+      <h2>Testando Tags HTML: H2</h2>
+      <h3>Testando Tags HTML: H3</h3>
+      <h4>Testando Tags HTML: H4</h4>
+      <h5>Testando Tags HTML: H5</h5>
+      <h6>Testando Tags HTML: H6</h6>
+    </div>
+  );
+}
+
+export default App;
