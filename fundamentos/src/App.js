0bf5b97e870a9906a4472a96606416306fbbb32a
Valmir <vmcfilho@gmail.com>
2023-03-24 18:42:55 -0300

    Update App.js

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -1,1 +1,18 @@
+// Importação do componente criado.
 
+// components
+import FirstComponent from './components/FisrtComponent';
+
+// styles - CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
