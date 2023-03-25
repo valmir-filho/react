07fea3e6f8998c8c116ed848eda86615c2a3fbe0
Valmir <vmcfilho@gmail.com>
2023-03-25 19:47:50 -0300

    Update RenderFunction.js

--- fundamentos/src/components/RenderFunction.js
+++ fundamentos/src/components/RenderFunction.js
@@ -1,1 +1,20 @@
+const RenderFunction = () => {
 
+    const renderSomething = (x) => {
+
+        if (x) {
+            return <h1>Se "x" é verdadeiro, renderizo isso!</h1>
+        } else {
+            return <h1>Senão isso!</h1>
+        }
+    }
+
+    return (
+        <div>
+            {renderSomething(true)}
+            {renderSomething(false)}
+        </div>
+    );
+};
+
+export default RenderFunction;
