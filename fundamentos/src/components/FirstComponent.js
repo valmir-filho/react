a4ec9ddd741c05d0c0bcde123f3be49cec29107a
Valmir <vmcfilho@gmail.com>
2023-03-25 18:51:05 -0300

    Update FirstComponent.js

--- fundamentos/src/components/FirstComponent.js
+++ fundamentos/src/components/FirstComponent.js
@@ -1,10 +1,13 @@
-// Criação de um componente.
+/* Criação de um componente.*/
+
+import MyComponent from "./MyComponent";
 
 const FirstComponent = () => {
 
     return (
         <div>
             <h1>Meu primeiro componente</h1>
+            <MyComponent /> {/* Reaproveitamento de componente*/}
         </div>
     );
 };
