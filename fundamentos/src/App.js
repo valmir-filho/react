a911e3ec4ee7f89c7edf4fef3d3fd186ae6481ea
Valmir <vmcfilho@gmail.com>
2023-03-25 19:19:10 -0300

    Update App.js

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -4,6 +4,7 @@
 import FirstComponent from './components/FirstComponent';
 import TemplateExpressions from './components/TemplateExpressions';
 import MyComponent from './components/MyComponent';
+import Events from './components/Events';
 
 // styles - CSS
 import './App.css';
@@ -16,6 +17,7 @@ function App() {
       <FirstComponent />
       <TemplateExpressions />
       <MyComponent />
+      <Events />
     </div>
   );
 }
