77798a98bd4c7eff03e48d5022d8e860a53d1578
Valmir <vmcfilho@gmail.com>
2023-03-30 15:26:49 -0300

    Update ListRender.js

--- avançando-no-react/src/components/ListRender.js
+++ avançando-no-react/src/components/ListRender.js
@@ -3,12 +3,18 @@ import { useState } from 'react';
 const ListRender = () => {
 
     const [list] = useState(["Valmir", "Audi", "Dudu"]);
-
-    const [users] = useState([
+    const [users, setUsers] = useState([
         { id: 1, name: "Valmir", age: 43 },
-        { id: 123121, name: "Marcia", age: 66 },
-        { id: 23423423, name: "Pedro", age: 44 },
+        { id: 2, name: "Marcia", age: 66 },
+        { id: 3, name: "Pedro", age: 44 },
     ]);
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+        setUsers((prevUsers) => {
+            console.log(prevUsers);
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        });
+    };
 
     return (
         <div>
@@ -18,6 +24,7 @@ const ListRender = () => {
             <ul>
                 {users.map((user) => <li key={user.id}>{user.name} - {user.age}</li>)}
             </ul>
+            <button onClick={deleteRandom}>Delete random user</button>
         </div >
     );
 };
