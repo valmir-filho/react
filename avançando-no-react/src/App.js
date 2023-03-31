6c4e54d0a8407e3140fd0325bfc728029ba12083
Valmir <vmcfilho@gmail.com>
2023-03-31 14:22:53 -0300

    Update App.js

--- avançando-no-react/src/App.js
+++ avançando-no-react/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import City from './assets/city.jpg';
+import { useState } from 'react';
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+
+  const name = "Valmir";
+  const [userName] = useState("Valmir");
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -19,6 +25,9 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name="Valmir" />  {/* Uma forma de adicionar propriedade. */}
+      <ShowUserName name={name} />  {/* Outra forma de adicionar propriedade. */}
+      <ShowUserName name={userName} />  {/* Outra forma de adicionar propriedade. */}
     </div>
   );
 };
