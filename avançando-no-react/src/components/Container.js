45bd0cb92477f044e5f8f63233a5e1187139c954
Valmir <vmcfilho@gmail.com>
2023-04-01 22:34:25 -0300

    Add files via upload

---
+++ avançando-no-react/src/components/Container.js
@@ -0,0 +1,11 @@
+const Container = ({ children }) => {
+
+    return (
+        <div>
+            <h2>Este é o título do container</h2>
+            {children}
+        </div>
+    );
+};
+
+export default Container;
