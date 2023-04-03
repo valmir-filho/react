7b15cf9eb2e546c1c0a21c576d32aa657de58b52
Valmir <vmcfilho@gmail.com>
2023-04-02 21:32:09 -0300

    Add files via upload

---
+++ avançando-no-react/src/components/ChangeMessageState.js
@@ -0,0 +1,12 @@
+const ChangeMessageState = ({ handleMessage }) => {
+    const messages = ["Oi", "Olá", "Olá, tudo bem?"];
+    return (
+        <div>
+            <button onClick={() => handleMessage(messages[0])}>1</button>
+            <button onClick={() => handleMessage(messages[1])}>2</button>
+            <button onClick={() => handleMessage(messages[2])}>3</button>
+        </div>
+    );
+};
+
+export default ChangeMessageState;
---
+++ avançando-no-react/src/components/Message.js
@@ -0,0 +1,10 @@
+const Message = ({ msg }) => {
+
+    return (
+        <div>
+            <h1>A mensagem é: {msg}</h1>
+        </div>
+    );
+};
+
+export default Message;
