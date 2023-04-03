0625aa356aa129b3fbe9eecb774aa4349c6c19e6
Valmir <vmcfilho@gmail.com>
2023-04-02 21:13:17 -0300

    Add files via upload

---
+++ avançando-no-react/src/components/ExecuteFunction.js
@@ -0,0 +1,11 @@
+const ExecuteFunction = ({ myFunction }) => {
+
+    return (
+        <div>
+            <button onClick={myFunction}>Clique aqui para executar a função!
+            </button>
+        </div>
+    );
+};
+
+export default ExecuteFunction;
