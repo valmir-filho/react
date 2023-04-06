f9241d3fbd4e08ad3246f28c13d20ea852531433
Valmir <vmcfilho@gmail.com>
2023-04-06 18:05:23 -0300

    Update App.js

--- css_react/src/App.js
+++ css_react/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+
+  const n = 6;
+
   return (
     <div className="App">
       {/* CSS global. */}
@@ -11,6 +14,9 @@ function App() {
       <p>Este parágrafo é do App.js</p>
       {/* Inline CSS. */}
       <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>Este elemento foi estilizado por meio do inline</p>
+      {/* Inline dinâmico CSS. */}
+      <h2 style={n < 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS dinâmico</h2>
+      <h2 style={n > 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS dinâmico</h2>
     </div>
   );
 }
