131e109f9fecfb699f426f0a29820aafc10382b2
Valmir <vmcfilho@gmail.com>
2023-04-10 10:54:43 -0300

    Create App.js

---
+++ form_em_react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyForm from './components/MyForm';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <MyForm />
+    </div>
+  );
+};
+
+export default App;
