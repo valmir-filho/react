88c4ef7a6ca8838a4817813b7a2a8d263f9c17c3
Valmir <vmcfilho@gmail.com>
2023-05-11 09:35:01 -0300

    Add files via upload

---
+++ miniblog-project/src/context/AuthContext.js
@@ -0,0 +1,12 @@
+// Context.
+import { useContext, createContext } from 'react';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children, value }) {
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuthValue() {
+    return useContext(AuthContext);
+}
