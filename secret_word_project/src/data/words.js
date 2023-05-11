db0d8c5c2f1e60f69e9908a1f04d7751f5509872
Valmir <vmcfilho@gmail.com>
2023-05-11 09:12:57 -0300

    Delete secret_word_project directory

--- secret_word_project/src/data/words.js
+++
@@ -1,8 +0,0 @@
-export const wordsList = {
-    carro: ["Ferrari", "Porsche", "Lamborghini", "Audi", "BMW", "Mercedez"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-    roupa: ["Calça", "Camisa", "Bermuda", "Camiseta", "Vestido"],
-    cor: ["Azul", "Amarelo", "Vermelho", "Verde", "Branco", "Preto"],
-};
