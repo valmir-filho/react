ca5cfc0153eab26d92215437b644bac2e7d27314
Valmir <vmcfilho@gmail.com>
2023-05-11 09:24:28 -0300

    Add files via upload

---
+++ miniblog-project/src/App.js
@@ -0,0 +1,60 @@
+// CSS.
+import './App.css';
+// React router.
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// Firebase.
+import { onAuthStateChanged } from 'firebase/auth';
+// Hooks.
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+// Context.
+import { AuthProvider } from './context/AuthContext';
+// Components.
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+// Pages.
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
+
+function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
+  return (
+    <div className="App">
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+              <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+              <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" />} />
+              <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+};
+
+export default App;
