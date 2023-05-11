cae3da8f74077ccf9f3248c0f8c0db304cbd8aef
Valmir <vmcfilho@gmail.com>
2023-05-11 09:34:25 -0300

    Add files via upload

---
+++ miniblog-project/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+//CSS.
+import styles from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <h3>Escreva sobre o que você mais gosta! 😎</h3>
+            <p>Mini Blog ©️ 2️⃣0️⃣2️⃣3️⃣</p>
+        </footer>
+    );
+};
+
+export default Footer;
