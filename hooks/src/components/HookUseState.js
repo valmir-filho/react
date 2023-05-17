74cd0067317472a4f14077b28b48911f505506bf
Valmir <vmcfilho@gmail.com>
2023-05-17 15:25:09 -0300

    Update HookUseState.js

--- hooks/src/components/HookUseState.js
+++ hooks/src/components/HookUseState.js
@@ -9,6 +9,13 @@ const HookUseState = () => {
         userName = 'João Silva';
         setName('Valmir Moro');
     };
+    // UseState and input.
+    const [age, setAge] = useState(18);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Send to an API.
+        console.log(age);
+    };
 
     return (
         <div>
@@ -17,6 +24,14 @@ const HookUseState = () => {
             <p>Variável: {userName} </p>
             <p>useState: {name} </p>
             <button onClick={changeNames}>Alterar nomes</button>
+            {/* UseState and input. */}
+            <p>Digite a sua idade:</p>
+            <form onSubmit={handleSubmit}>
+                {/* Controlled input. */}
+                <input type='text' value={age} onChange={e => setAge(e.target.value)} />
+                <input type='submit' value='Enviar' />
+                <p>Você tem {age} anos.</p>
+            </form>
             <hr />
         </div>
     );
