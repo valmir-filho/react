b27db249458ed16988bb370fa2892a288b65f826
Valmir <vmcfilho@gmail.com>
2023-05-18 13:19:23 -0300

    Update Home.js

--- hooks/src/pages/Home/Home.js
+++ hooks/src/pages/Home/Home.js
@@ -1,10 +1,14 @@
-// Hook.
-import HookUseState from "../../components/HookUseState";
+// Hooks.
+import HookUseReducer from '../../components/HookUseReducer';
+import HookUseState from '../../components/HookUseState';
+import HookUseEffect from '../../components/HookUseEffect';
 
 const Home = () => {
     return (
         <div>
             <HookUseState />
+            <HookUseReducer />
+            <HookUseEffect />
         </div>
     );
 };
