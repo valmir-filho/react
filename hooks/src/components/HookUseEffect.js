00f6ab30326a61b65b2bc77c5ac1e87ee95c1489
Valmir <vmcfilho@gmail.com>
2023-05-18 13:20:16 -0300

    Add files via upload

---
+++ hooks/src/components/HookUseEffect.js
@@ -0,0 +1,22 @@
+// React.
+import { useEffect, useState } from 'react';
+
+const HookUseEffect = () => {
+    // UseEffect without dependencies.
+    useEffect(() => {
+        console.log('Estou sendo executado.');
+    });
+    const [number, setNumber] = useState(1);
+    const changeSomething = () => { setNumber(number + 1) };
+
+    return (
+        <div>
+            <h2>useEffect</h2>
+            <p>Número: {number}</p>
+            <button onClick={changeSomething}>Executar!</button>
+            <hr />
+        </div>
+    );
+};
+
+export default HookUseEffect;
