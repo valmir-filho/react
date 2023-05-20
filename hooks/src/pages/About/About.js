f360232158040748f4a315fd7f8edaba9ee1c586
Valmir <vmcfilho@gmail.com>
2023-05-20 20:57:44 -0300

    Update About.js

--- hooks/src/pages/About/About.js
+++ hooks/src/pages/About/About.js
@@ -1,6 +1,15 @@
+// context.
+import { useContext } from 'react';
+import { SomeContext } from '../../components/HookUseContext';
+
 const About = () => {
+    const { contextValue } = useContext(SomeContext);
+
     return (
-        <div>About</div>
+        <div>
+            <h2>About</h2>
+            <p>Valor do contexto: {contextValue}</p>
+        </div>
     );
 };
 
