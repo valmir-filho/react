ad649399404dbc3dd521a141f1cafbbf0d0e5fcc
Valmir <vmcfilho@gmail.com>
2023-05-20 20:03:59 -0300

    Update HookUseEffect.js

--- hooks/src/components/HookUseEffect.js
+++ hooks/src/components/HookUseEffect.js
@@ -10,14 +10,21 @@ const HookUseEffect = () => {
     const changeSomething = () => { setNumber(number + 1) };
     // UseEffect with empty dependencies array.
     useEffect(() => {
-        console.log('Serei executado apenas uma vez!')
+        console.log('Serei executado apenas uma vez!');
     }, []);
-
+    // UseEffect with values in the dependencies array.
+    const [anotherNumber, setAnotherNumber] = useState(0);
+    useEffect(() => {
+        console.log('Serei executado apenas quando o anotherNumber muda!');
+    }, [anotherNumber]);
+    
     return (
         <div>
             <h2>useEffect</h2>
             <p>Número: {number}</p>
             <button onClick={changeSomething}>Executar!</button>
+            <p>Another Number: {anotherNumber}</p>
+            <button onClick={() => setAnotherNumber(anotherNumber + 1)}>Mudar o anotherNumber!</button>
             <hr />
         </div>
     );
