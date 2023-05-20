e7f362d3829e00d86c707695248e3e93325245e9
Valmir <vmcfilho@gmail.com>
2023-05-20 20:55:52 -0300

    Create HookUseContext.js

---
+++ hooks/src/components/HookUseContext.js
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+
+export const SomeContext = createContext();
+export const HookUseContext = ({ children }) => {
+    const contextValue = 'testing context';
+
+    return (<SomeContext.Provider value={{ contextValue }}>{children}</SomeContext.Provider>);
+};
