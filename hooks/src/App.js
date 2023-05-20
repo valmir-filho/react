3615eb88c02ad0cd36857fd4e47a2ed0f0cd433d
Valmir <vmcfilho@gmail.com>
2023-05-20 20:56:42 -0300

    Update App.js

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 // React router.
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// Hook.
+import { HookUseContext } from './components/HookUseContext';
 // Pages.
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -9,21 +11,23 @@ import About from './pages/About/About';
 function App() {
   return (
     <div className="App">
-      <h1>Hooks</h1>
-      <BrowserRouter>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <HookUseContext>
+        <h1>Hooks</h1>
+        <BrowserRouter>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </HookUseContext>
     </div>
   );
 }
