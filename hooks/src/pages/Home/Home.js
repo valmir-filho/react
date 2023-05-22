8cd835df691ebb277e6d758201d9df2a3beabdee
Valmir <vmcfilho@gmail.com>
2023-05-22 13:21:02 -0300

    Update Home.js

--- hooks/src/pages/Home/Home.js
+++ hooks/src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import React from 'react';
 import HookUseReducer from '../../components/HookUseReducer';
 import HookUseState from '../../components/HookUseState';
 import HookUseEffect from '../../components/HookUseEffect';
+import HookUseRef from '../../components/HookUseRef';
 // Context.
 import { useContext } from 'react';
 import { SomeContext } from '../../components/HookUseContext';
@@ -19,6 +20,7 @@ const Home = () => {
             <h2>useContext</h2>
             <p>Valor do context: {contextValue}</p>
             <hr />
+            <HookUseRef />
         </div>
     );
 };
