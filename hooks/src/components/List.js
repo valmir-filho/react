84aa95f64ebec26ef885c0fa45d102b53d88fe27
Valmir <vmcfilho@gmail.com>
2023-05-23 14:35:27 -0300

    Add files via upload

---
+++ hooks/src/components/List.js
@@ -0,0 +1,14 @@
+// React.
+import { useState, useEffect } from 'react';
+
+const List = ({ getItems }) => {
+    const [myItems, setMyItems] = useState([]);
+    useEffect(() => {
+        console.log('Buscando itens no DB...');
+        setMyItems(getItems());
+    }, [getItems]);
+
+    return (<div>{myItems.map((item) => (<p key={item}>{item}</p>))}</div>);
+};
+
+export default List;
