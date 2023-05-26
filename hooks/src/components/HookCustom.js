c7fe3a239ed969f0c9ad60159cd843f8e54f41f3
Valmir <vmcfilho@gmail.com>
2023-05-26 13:43:33 -0300

    Create HookCustom.js

---
+++ hooks/src/components/HookCustom.js
@@ -0,0 +1,21 @@
+// React.
+import { useDebugValue, useState } from 'react';
+// Hook.
+import { usePrevious } from '../hooks/usePrevious';
+
+const HookCustom = () => {
+    const [number, setNumber] = useState(0);
+    const previousValue = usePrevious(number);
+
+    return (
+        <div>
+            <h2>Custom Hook</h2>
+            <p>Atual: {number}</p>
+            <p>Anterior: {previousValue}</p>
+            <button onClick={() => setNumber(Math.random())}>Alterar!</button>
+            <hr />
+        </div>
+    );
+};
+
+export default HookCustom;
