3ef6ff654df30d8c8ffb3b4e971ba4ca456c3c51
Valmir <vmcfilho@gmail.com>
2023-05-26 13:42:17 -0300

    Update Home.js

--- hooks/src/pages/Home/Home.js
+++ hooks/src/pages/Home/Home.js
@@ -12,6 +12,7 @@ import HookUseCallback from '../../components/HookUseCallback';
 import HookUseMemo from '../../components/HookUseMemo';
 import HookUseLayoutEffect from '../../components/HookUseLayoutEffect';
 import HookUseImperativeHandle from '../../components/HookUseImperativeHandle';
+import HookCustom from '../../components/HookCustom';
 
 const Home = () => {
     const { contextValue } = useContext(SomeContext);
@@ -29,6 +30,7 @@ const Home = () => {
             <HookUseMemo />
             <HookUseLayoutEffect />
             <HookUseImperativeHandle />
+            <HookCustom />
         </div>
     );
 };
