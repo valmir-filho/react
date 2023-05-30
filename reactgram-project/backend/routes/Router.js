4fa07564abda150a67b0f5d11e40b6179a243454
Valmir <vmcfilho@gmail.com>
2023-05-30 14:56:28 -0300

    Update Router.js

--- reactgram-project/backend/routes/Router.js
+++ reactgram-project/backend/routes/Router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express();
 
+router.use('/api/users', require('./UserRoutes'));
+
 // Test route.
-router.get('/', (req, res) => {
-    res.send('API working!');
-});
+router.get('/', (req, res) => { res.send('API working!') });
 
 module.exports = router;
