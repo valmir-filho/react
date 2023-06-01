760f6a9fbb66d6f472d8dd9ea3da4992a29e656d
Valmir <vmcfilho@gmail.com>
2023-06-01 12:18:52 -0300

    Update UserController.js

--- reactgram-project/backend/controllers/UserController.js
+++ reactgram-project/backend/controllers/UserController.js
@@ -10,9 +10,76 @@ const generateToken = (id) => {
 };
 // Register user and sign in.
 const register = async (req, res) => {
-    res.send('Registro');
+    const { name, email, password } = req.body;
+    // Check if user exists.
+    const user = await User.findOne({ email });
+
+    if (user) {
+        res.status(422).json({ errors: ['😵‍💫 Erro! E-mail já cadastrado! Por favor, utilize outro.'] });
+        return;
+    }
+
+    // Generate password hash.
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    // Create user.
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash,
+    });
+
+    // Check if the user was successfully created. If yes, return the token.
+    if (!newUser) {
+        res.status(422).json({
+            errors: ['😵‍💫 Erro! Por favor, tente novamente mais tarde.'],
+        });
+        return;
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
+};
+// Sign user in.
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    // Check if user exists.
+    if (!user) {
+        res.status(404).json({ errors: ['😵‍💫 Erro! Usuário não encontrado.'] });
+        return;
+    }
+
+    // Check if password matches.
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({ errors: ['😵‍💫 Erro! Senha inválida'] });
+        return;
+    }
+
+    // Return user with your token.
+    res.status(200).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id),
+    });
+};
+// Get logged in user.
+const getCurrentUser = async (req, res) => {
+    const user = req.user;
+
+    res.status(200).json(user);
+};
+// Update an user.
+const update = async (req, res) => {
+    res.send('Update');
 };
 
 module.exports = {
     register,
+    login,
+    getCurrentUser,
+    update,
 };
