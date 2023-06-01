90ce456dbde258a6677c9a4f27903273b78d7e36
Valmir <vmcfilho@gmail.com>
2023-06-01 12:23:47 -0300

    Update UserRoutes.js

--- reactgram-project/backend/routes/UserRoutes.js
+++ reactgram-project/backend/routes/UserRoutes.js
@@ -4,7 +4,16 @@ const router = express.Router();
 // Controller.
 const { register, getCurrentUser, login, update, getUserById } = require('../controllers/UserController');
 
+// Middlewares.
+const validate = require('../midlewares/handleValidation');
+const { userCreateValidation, loginValidation, userUpdateValidation } = require('../midlewares/userValidations');
+const authGuard = require('../midlewares/authGuard');
+const { imageUpload } = require('../midlewares/imageUpload');
+
 // Routes.
-router.post('/register', register);
+router.post('/register', userCreateValidation(), validate, register);
+router.post('/login', loginValidation(), validate, login);
+router.get('/profile', authGuard, getCurrentUser);
+router.put('/', authGuard, userUpdateValidation(), validate, imageUpload.single('profileImage'), update);
 
 module.exports = router;
