6bca13956314f41b469a962effa8b0d2227bd1b7
Valmir <vmcfilho@gmail.com>
2023-06-04 19:21:28 -0300

    Update UserController.js

--- reactgram-project/backend/controllers/UserController.js
+++ reactgram-project/backend/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const jwtSecret = process.env.JWT_SECRET;
 // Generate user token.
 const generateToken = (id) => {
@@ -74,7 +75,49 @@ const getCurrentUser = async (req, res) => {
 };
 // Update an user.
 const update = async (req, res) => {
-    res.send('Update');
+    const { name, password, bio } = req.body;
+    let profileImage = null;
+
+    if (req.file) {
+        profileImage = req.file.filename;
+    }
+
+    const reqUser = req.user;
+    const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select('-password');
+
+    if (name) {
+        user.name = name;
+    }
+
+    if (password) {
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+        user.password = passwordHash;
+    }
+
+    if (profileImage) {
+        user.profileImage = profileImage;
+    }
+
+    if (bio) {
+        user.bio = bio;
+    }
+
+    await user.save();
+
+    res.status(200).json(user);
+};
+// Get user by id
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select('-password');
+    // Check if user exists.
+    if (!user) {
+        res.status(404).json({ errors: ['😵‍💫 Erro! Usuário não encontrado!'] });
+        return;
+    }
+
+    res.status(200).json(user);
 };
 
 module.exports = {
@@ -82,4 +125,5 @@ module.exports = {
     login,
     getCurrentUser,
     update,
+    getUserById,
 };
