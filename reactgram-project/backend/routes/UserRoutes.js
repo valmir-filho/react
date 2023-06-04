49d8d6aaa5d2b8a322b8ab78a6cc680d24583701
Valmir <vmcfilho@gmail.com>
2023-06-04 19:35:59 -0300

    Update UserRoutes.js

--- reactgram-project/backend/routes/UserRoutes.js
+++ reactgram-project/backend/routes/UserRoutes.js
@@ -15,5 +15,6 @@ router.post('/register', userCreateValidation(), validate, register);
 router.post('/login', loginValidation(), validate, login);
 router.get('/profile', authGuard, getCurrentUser);
 router.put('/', authGuard, userUpdateValidation(), validate, imageUpload.single('profileImage'), update);
+router.get('/:id', getUserById);
 
 module.exports = router;
