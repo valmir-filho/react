24cf0947674aa6cca0ede87e429b55877d04b9e0
Valmir <vmcfilho@gmail.com>
2023-06-04 19:35:28 -0300

    Create PhotoRoutes.js

---
+++ reactgram-project/backend/routes/PhotoRoutes.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+
+// Controller.
+const {
+    insertPhoto,
+    deletePhoto,
+    getAllPhotos,
+    getUserPhotos,
+    getPhotoById,
+    updatePhoto,
+    likePhoto,
+    commentPhoto,
+    searchPhotos,
+} = require('../controllers/PhotoController');
+
+// Middlewares.
+const authGuard = require('../middlewares/authGuard');
+const validate = require('../middlewares/handleValidations');
+const {
+    photoInsertValidation,
+    photoUpdateValidation,
+    commentValidation,
+} = require('../middlewares/photoValidations');
+const { imageUpload } = require("../middlewares/imageUpload");
+
+// Routes.
+router.post(
+    '/',
+    authGuard,
+    imageUpload.single('image'),
+    photoInsertValidation(),
+    validate,
+    insertPhoto
+);
+router.delete('/:id', authGuard, deletePhoto);
+router.get('/', getAllPhotos);
+router.get('/user/:id', getUserPhotos);
+router.get('/search', searchPhotos);
+router.get('/:id', getPhotoById);
+router.put(
+    '/:id',
+    authGuard,
+    imageUpload.single('image'),
+    photoUpdateValidation(),
+    validate,
+    updatePhoto
+);
+router.put('/like/:id', authGuard, likePhoto);
+router.put(
+    '/comment/:id',
+    authGuard,
+    commentValidation(),
+    validate,
+    commentPhoto
+);
+
+module.exports = router;
