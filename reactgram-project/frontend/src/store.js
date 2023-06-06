3d7ce5d8d5cd2b7f3d0c4651db9266a9b4906c31
Valmir <vmcfilho@gmail.com>
2023-06-06 13:45:06 -0300

    Update store.js

--- reactgram-project/frontend/src/store.js
+++ reactgram-project/frontend/src/store.js
@@ -1,7 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
-import userReducer from "./slices/userSlice";
-import photoReducer from "./slices/photoSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import userReducer from './slices/userSlice';
+import photoReducer from './slices/photoSlice';
 
 export const store = configureStore({
     reducer: {
